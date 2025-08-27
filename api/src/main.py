7c1e3b3243f85e2c8bb7cb5f6b1b5d9e241b5623
Sergey Smolnikov <ssmol@equinor.com>
2025-08-27 16:12:13 +0200

    Added put command to api

--- api/src/main.py
+++ api/src/main.py
@@ -1,6 +1,8 @@
 from typing import Union
 from fastapi import FastAPI
 
+from api.src.models.item import Item
+
 app = FastAPI()
 @app.get("/")
 def read_root():
@@ -9,3 +11,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def get_item_by_id(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item_name": item.name}
